938251c866c231100a2330a7649d723ae9964475
martgarcon <miefrzevv@gmail.com>
2026-03-08 07:33:32 +0300

    git commit -m "fix: single entry point for Vite build"

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        about: resolve(__dirname, "about.html"),
-        work: resolve(__dirname, "work.html"),
-        project: resolve(__dirname, "project.html"),
-        contact: resolve(__dirname, "contact.html"),
-      },
+      input: "index.html",
     },
     assetsInclude: [
       "**/*.jpeg",
